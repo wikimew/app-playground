56e2235c538370d47138d9426a89d989f2db4701
wikimew <75874671+wikimew@users.noreply.github.com>
2024-01-27 09:37:09 +0000

    FIX: navigation

--- ui/global-nav.tsx
+++ ui/global-nav.tsx
@@ -8,6 +8,7 @@ import { MenuAlt2Icon, XIcon } from '@heroicons/react/solid';
 import clsx from 'clsx';
 import { useState } from 'react';
 import Byline from './byline';
+import { useRouter } from 'next/router';
 
 export function GlobalNav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -83,11 +84,13 @@ function GlobalNavItem({
 }) {
   const segment = useSelectedLayoutSegment();
   const isActive = item.slug === segment;
-
+  const router = useRouter();
   return (
-    <Link
-      onClick={close}
-      href={`/${item.slug}`}
+    <div
+      onClick={() => {
+        close();
+        router.push(`/${item.slug}`);
+      }}
       className={clsx(
         'block rounded-md px-3 py-2 text-sm font-medium hover:text-gray-300',
         {
@@ -97,6 +100,6 @@ function GlobalNavItem({
       )}
     >
       {item.name}
-    </Link>
+    </div>
   );
 }
